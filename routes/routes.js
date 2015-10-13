cb3e8e89acccd64744df1a4d8e9b5ffe6664a401
Ali <saberi.ar.eng@gmail.com>
2015-10-13 14:58:57 -0400

    feat: GET request to database is set up
       Get request to the database is set up. so when sina calls database api all off
    object in it should be returned

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(app, express, fortune){
+module.exports = function(app, express, db, fortune){
 	app.set('view engine', 'ejs');
 	app.use(express.static(__dirname + 'public'));
 	app.get('/', function(req, res, next){
@@ -14,6 +14,14 @@ module.exports = function(app, express, fortune){
 	app.get('/fortune', function(req, res, next){
 		res.send(fortune.getFortune());
 	});
+
+	app.get('/database', function(req, res, next){
+		  db.candidateList.find(function (err, docs) {
+    	  console.log(docs);
+          res.json(docs);
+  });
+	});
+
 	// catching 404 error
 	app.use(function(req, res, next){
 		res.status(404);
