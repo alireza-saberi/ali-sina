59f6fca8961d3fc40bdac257f3b9069ad23447e6
alireza-saberi <saberi.ar.eng@gmail.com>
2015-11-08 11:15:46 -0500

    feat: put is added
    put function is added to modify especific object among users

--- routes/routes.js
+++ routes/routes.js
@@ -20,29 +20,29 @@ module.exports = function(app, express, db, fortune, bodyParser){
 		res.render('about');
 	});
 	app.get('/sina', function(req, res, next){
-		console.log("Server: I got a Sina App request");
+		console.log("Server: I got a Sina app request");
 		res.send('Hello Sina');
 	});
 
 	app.get('/fortune', function(req, res, next){
-		console.log("Server: I got a fortune App request");
+		console.log("Server: I got a fortune app request");
 		res.send(fortune.getFortune());
 	});
 
 	app.get('/getList', function(req, res, next){
-		console.log("Server: I got a getList App request");
+		console.log("Server: I got a getList app request");
 		res.json(users);
 	});
 
 	app.get('/getList:id', function(req, res, next){
 		var id = req.params.id;
-		console.log("Server: I got a getList App request for user with this id: ", id );
+		console.log("Server: I got a getList app request for user with this id: ", id );
 		res.json(users[id]);
 	});
 
 	app.get('/checker', function(req, res, next){
 		var name = req.body.name;
-		console.log("Server: I got a checker App request this user: ", name );
+		console.log("Server: I got a checker app request this user: ", name );
 		if (name == "Cyrus") {
 			res.send('Right name man!');
 							}else
@@ -52,17 +52,25 @@ module.exports = function(app, express, db, fortune, bodyParser){
 	});
 
 	app.post('/add', function(req, res){
-		console.log("Server: I got add App request this shit: ", req.body);
+		console.log("Server: I got add app request this shit: ", req.body);
 		users.push(req.body);
 		res.json(users);
 	});
 
 	app.delete('/delete:id', function(req, res){
 		var id = req.params.id;
-		console.log("Server: I got delete App request this shit: ", id);
+		console.log("Server: I got delete app request this shit: ", id);
 		users.splice(id, 1);
 		res.json(users);
 
+	});
+
+	app.put('/modify:id', function(req, res){
+		var id = req.params.id;
+		console.log("Server: I got a modify app request this shit: ", id);
+		users[id] = {};
+		users[id] = req.body;
+		res.json(users);
 	});
 
 
